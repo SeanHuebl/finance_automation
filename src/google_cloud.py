ed40200c6334d3f1576c84e6f4e0592e61e1e6f8
Sean Huebl <sean.huebl@gmail.com>
2024-10-22 06:02:29 -0600

    started working on the google cloud / sheets access. Learned about google secret manager so I can securely store the oath credentials. The OAuth2.0 flow is the authorization that pops up a window and prompts the user to allow access. Now that we can obtain the credentials we should be allowed to read/write to google sheets which will be the next focus

---
+++ src/google_cloud.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+from google.cloud import secretmanager
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+PROJECT_ID = 'divine-fuze-439311-d6'
+SECRET_ID = 'sheets_access'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+def access_secret(version_id: str = 'latest') -> json:
+    
+    client = secretmanager.SecretManagerServiceClient()
+    name = f'projects/{PROJECT_ID}/secrets/{SECRET_ID}/versions/{version_id}'
+
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode('utf-8')
+
+def get_credentials() -> Credentials:
+    client_secret = access_secret()
+
+    with open('temp_client_secret.json', 'w') as secret_file:
+        secret_file.write(client_secret)
+    
+    flow = InstalledAppFlow.from_client_secrets_file('temp_client_secret.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+
+    os.remove('temp_client_secret.json')
+
+    return creds
