55337850a712472c7e4f9d1cf8c5fd81bf0bac24
Sean Huebl <sean.huebl@gmail.com>
2024-10-23 06:08:31 -0600

    working on the cloud access, seems I can't access the secret for authentication

--- src/google_cloud.py
+++ src/google_cloud.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 
 from google.cloud import secretmanager
@@ -5,30 +6,41 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import Resource
 
-PROJECT_ID = 'divine-fuze-439311-d6'
+PROJECT_ID = '438040797905'
 SECRET_ID = 'sheets_access'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-def access_secret(version_id: str = 'latest') -> str:
+def access_secret(version_id: str = 'lastest') -> str:
     
     client = secretmanager.SecretManagerServiceClient()
     name = f'projects/{PROJECT_ID}/secrets/{SECRET_ID}/versions/{version_id}'
 
     response = client.access_secret_version(name=name)
-    return response.payload.data.decode('utf-8')
+    return response.payload.data.decode('UTF-8')
 
 def get_credentials() -> Credentials:
     client_secret = access_secret()
 
-    with open('temp_client_secret.json', 'w') as secret_file:
+    with open('./temp_client_secret.json', 'w') as secret_file:
         secret_file.write(client_secret)
     
-    flow = InstalledAppFlow.from_client_secrets_file('temp_client_secret.json', SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file('./temp_client_secret.json', SCOPES)
     creds = flow.run_local_server(port=0)
 
-    os.remove('temp_client_secret.json')
+    os.remove('./temp_client_secret.json')
 
     return creds
 
-def write_data(service: Resource, sheet_name: str, spreadsheet_id: str) -> None:
-    
+def write_data(service: Resource, sheet_name: str, spreadsheet_id: str, df: pd.DataFrame) -> None:
+    category = sheet_name.upper()
+    df = df[df['Category'] == category].copy().drop(['Transaction', 'Category'], axis=1)
+    values = df.apply(lambda row: row.tolist(), axis=1).tolist()
+    range_name = f'{sheet_name}!A2:C'
+    body = {'values' : values}
+    results = service.spreadsheets().values().update(
+        spreadsheet_Id=spreadsheet_id, 
+        range=range_name,
+        valueInputOption='USER_ENTERED',
+        body=body
+        ).execute()
+    return f"{results.get('updatedCells')} cells updated"
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from googleapiclient.discovery import build
-from google_cloud import get_credentials
+from google_cloud import get_credentials, write_data
 from wrangle_data import wrangle_data
 
 def main():
@@ -12,7 +12,8 @@ def main():
     credentials = get_credentials()
     service = build('sheets', 'v4', credentials=credentials)
     spreadsheet_id = '1T_-oDRsrrKe0pmfGlSkh59K80fc3AmMVJphW7OEh09A'
-
+    result = write_data(service, 'Food', spreadsheet_id, df)
+    print(result)
     pd.reset_option('display.max_rows')
 
 if __name__ == '__main__':
